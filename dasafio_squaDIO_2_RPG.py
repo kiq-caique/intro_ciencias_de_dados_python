74947a2e0148dd5de6b1bca315932817d1acbc86
kiq-caique <96061515+kiq-caique@users.noreply.github.com>
2024-05-27 21:33:00 -0300

    Add files via upload

---
+++ dasafio_squaDIO_2_RPG.py
@@ -0,0 +1,11 @@
+# Lista para armazenar os itens
+itens = []
+
+for i in range(3):
+    item = input()
+    itens.append(item)
+
+
+print("Lista de itens:")
+for item in itens:
+    print(f"- {item}")
